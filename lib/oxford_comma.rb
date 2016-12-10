2bc72e57db09f94447348040b402f5eaa1d0564a
Abdalkhalek ahmed <eng.abdalkalk@gmail.com>
2016-12-10 20:56:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size==1
+    return array.join
+  elsif array.size==2
+     return array = "#{array[0]} and #{array[1]}"
+elsif array.size > 2
+postion = array.pop
+array.push("and #{postion}")
+array.join(", ")
 end
+end
+puts oxford_comma(["sdsd","dfdfd","dfdfdf","fff"])
